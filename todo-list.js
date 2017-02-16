ede2a05ebc7ab4176f1ae9e594b5bc9790ec86d7
Yehoshua Kahan <yehoshua.kahan.training@gmail.com>
2017-02-16 16:34:57 +0200

    Improved todo list

--- todo-list.js
+++ todo-list.js
@@ -4,15 +4,41 @@ var input = "start"
 while (input !== "quit") {
 //handle input
 if (input === "list") {
-    console.log(todos);
+    listTodos();
 } else if (input === "new") {
-  // ask for new item
-  var newTodo = prompt("What are we adding, boss?")
-  // add newTodo to the todos array
-  todos.push(newTodo);
+    addTodos();
+} else if (input === "delete") {
+    deleteTodos();
 }
 
 //ask for more new input
 input = prompt("What do you want to do today?");
 }
+
 alert("Ok, you're out");
+
+function listTodos() {
+    console.log("************");
+    todos.forEach(function(todo, i){
+    console.log(i + ": " + todo);
+    });
+    console.log("************");
+    }
+
+function addTodos() {
+   // ask for new item
+   var newTodo = prompt("What are we adding, boss?")
+   // add newTodo to the todos array
+   todos.push(newTodo);
+   alert("Ok, it's added.");
+  }
+
+function deleteTodos() {
+  //ask for target index
+  var index = prompt("I'll need an index, boss");
+
+  //delete target
+  //splice()
+  todos.splice(index, 1);
+  alert("Item Deleted!");
+  }
