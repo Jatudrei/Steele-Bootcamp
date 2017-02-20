01ba20a659aeebda2ac51b5f8dfac66be8fbf130
Yehoshua Kahan <yehoshua.kahan.training@gmail.com>
2017-02-20 16:25:02 +0200

    dom-demo modified

--- dom-demo.js
+++ dom-demo.js
@@ -2,11 +2,22 @@ var button = document.querySelector("button");
 var par = document.querySelector("#click");
 var clicked = false;
 
-//Setup Click Listener
+//Setup Click Listener on Button
 button.addEventListener("click", function() {
 
-   if (!clicked) {par.textContent = "I can't believe it!  They finally clicked me!";}
-   if (clicked) {par.textContent = "They unclicked me.  I hate my life.";}
+   if (!clicked) {par.innerHTML = "I can't believe it!  They finally clicked me!  <strong>Blurt!</strong>";}
+   else {par.textContent = "They unclicked me.  I hate my life.";}
    clicked = !clicked;
 
    });
+
+// Setup Click Listener on LIs
+var liClick = false;
+
+var lis = document.querySelectorAll("li");
+for (var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function() {
+        this.style.background="pink";
+    });
+}
+
