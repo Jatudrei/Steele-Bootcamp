8fe4ba8d72a00b39490e3364068bf1693f0e8d79
Yehoshua Kahan <yehoshua.kahan.training@gmail.com>
2017-02-27 18:30:18 +0200

    Todo List subtantively done, styling partially done

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,15 +1,25 @@
 // Check off Specific Items by Clicking
-$("li").click(function(){
+$("ul").on("click", "li", (function(){
     // toggle items from gray and struck-through to black and not struck-through
     $(this).toggleClass("completed");
-});
+}));
 
-$("span").click(function(event){
+$("ul").on("click", "span", (function(event){
     // stop Event Bubbling
     event.stopPropagation();
     // click on X to delete item
     $(this).parent().fadeOut(400, function(){
         $(this).remove();
-});
-});
+    });
+}));
 
+$("input[type='text']").keypress(function(event){
+    if (event.which===13){
+        // recording todo text from input box
+        var todoText = $(this).val();
+        // emptying input box
+        $(this).val("");;
+        // create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
